9a70c0b552d4b5cdfd841fe8f29c5d07250eda0f
wuyanshen <763010522@qq.com>
2019-07-29 23:44:56 +0800

    首次提交

---
+++ src/components/MenuCell.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="menu-cell">
+        <router-link :to="url">
+            <div class="img-wrap"><img :src="imgUrl" :alt="menuName" /></div>
+            <div class="title-wrap">{{menuName}}</div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "menucell",
+        props: ["menuName", "imgUrl", "url"],
+        data() {
+            return {
+
+            }
+        },
+        components: {
+
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .menu-cell {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        font-size: $text-size;
+        text-align: center;
+
+        .title-wrap {
+            color: #000;
+        }
+
+        .img-wrap {
+            width: px2rem(68);
+            height: px2rem(68);
+            display: inline-block;
+            margin-bottom: px2rem(36);
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout">
+  <div class="home">
     <headtop title="SFA" backurl="">
       <router-link to="/user" slot="r" class="icon iconfont icon-user"></router-link>
     </headtop>
@@ -11,6 +11,15 @@
         <mprogress title="有效商店数" :val="shops" :percent="false"></mprogress>
       </div>
     </div>
+    <div class="main-nav">
+      <div class="nav-cell" v-for="i in 9" :key="i">
+        <template v-if="i<=menuList.length">
+          <menucell :url="menuList[i-1].url" :img-url="menuList[i-1].imgUrl" :menu-name="menuList[i-1].menuName">
+          </menucell>
+        </template>
+
+      </div>
+    </div>
 
   </div>
 </template>
@@ -20,18 +29,63 @@
   import headtop from '../components/TopHead';
   import mprogress from '../components/Progress';
   import service from '../service';
+  import menucell from '../components/MenuCell';
+
+  const menuList = [{
+      "menuName": "公司通告",
+      "imgUrl": require("@/assets/img/gstg.png"),
+      "url": "/notice"
+    },
+    {
+      "menuName": "进店拜访",
+      "imgUrl": require("@/assets/img/jdbf.png"),
+      "url": "/visitshop"
+    },
+    {
+      "menuName": "电话订单",
+      "imgUrl": require("@/assets/img/dhdd.png"),
+      "url": "/phoneorder"
+    },
+    {
+      "menuName": "订单状态",
+      "imgUrl": require("@/assets/img/ddzt.png"),
+      "url": "/orders"
+    },
+    {
+      "menuName": "培训资料",
+      "imgUrl": require("@/assets/img/pxzl.png"),
+      "url": "/training"
+    },
+    {
+      "menuName": "消息中心",
+      "imgUrl": require("@/assets/img/xxzx.png"),
+      "url": "/message"
+    },
+    {
+      "menuName": "新增门店",
+      "imgUrl": require("@/assets/img/xzmd.png"),
+      "url": "/addedshop"
+    },
+    {
+      "menuName": "同步数据",
+      "imgUrl": require("@/assets/img/sjtb.png"),
+      "url": "/asyncdata"
+    }
+  ];
 
   export default {
     name: "home",
     data() {
       return {
         monthSalePercent: 0,
-        shops: 0
+        shops: 0,
+        menuList
       }
     },
     components: {
       headtop,
-      mprogress
+      mprogress,
+      menucell
     },
     computed: {
       getYearMonth() {
@@ -56,6 +110,33 @@
 
 
 <style lang="scss" scoped>
+  .home {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    .main-nav {
+      flex: 1;
+      padding: 0 px2rem(30);
+      display: flex;
+      flex-flow: row wrap;
+      align-content: stretch;
+      align-items: stretch;
+      background-color: #fff;
+      border-top: 1px solid #ccc;
+
+      .nav-cell {
+        border-bottom: 1px solid #ccc;
+        border-right: 1px solid #ccc;
+        flex: 1 1 30%;
+      }
+
+      .nav-cell:nth-child(3n) {
+        border-right: none;
+      }
+    }
+  }
+
   #pie {
     width: px2rem(500);
     height: px2rem(400);
